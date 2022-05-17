bda9cd7651bcd34594175950115e1b7055486995
nguyen.aislab <nguyen.aislab@gmail.com>
2022-05-18 08:52:33 +0900

    update server url for proxy

--- config/server.js
+++ config/server.js
@@ -1,7 +1,9 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: 'https://aislab.tech/internal',
+  proxy: true,
   app: {
     keys: env.array('APP_KEYS'),
-  },
+  }
 });
